162d0a0f9db0c809a57a9a8777cc7ce4225f6f17
Granier Sylvain <151046639+SylvainEODD@users.noreply.github.com>
2026-02-02 09:29:02 +0100

    On ajoute des tests pour augmenter le score codecov

--- backend/api/tests/conftest.py
+++ backend/api/tests/conftest.py
@@ -3,6 +3,11 @@ from rest_framework.test import APIClient
 
 
 @pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
 def api_client_auth(normal_user):
     client = APIClient()
     client.force_authenticate(user=normal_user)
